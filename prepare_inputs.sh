87b6185774cf412519fe48640c91d829510f73fb
Michael Kotliar <misha.kotliar@gmail.com>
2018-08-13 16:56:00 -0400

    Update script to extract FASTQ
    Can be run from any folder

--- prepare_inputs.sh
+++ prepare_inputs.sh
@@ -1,1 +1,3 @@
-cat ./inputs/SRR1198790_* | gzip -d > ./inputs/SRR1198790.fastq && rm ./inputs/SRR1198790_*
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cat "$DIR"/inputs/SRR1198790_* | gzip -d > "$DIR"/inputs/SRR1198790.fastq && rm "$DIR"/inputs/SRR1198790_*
